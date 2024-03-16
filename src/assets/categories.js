e631645552324260ebe6034acda818cdce59d069
Christopher Steven <christopherstevennunoo@gmail.com>
2024-03-16 11:40:09 +0000

    adding categories data for Categories card

---
+++ src/assets/categories.js
@@ -0,0 +1,37 @@
+import beverages from './beverages.png'
+import diary_and_eggs from './diary-and-eggs.png'
+import fresh_produce from './fresh-produce.png'
+import meat_and_seafood from './meat-and-seafood.png'
+import frozen_foods from './frozen-foods.png'
+
+
+
+const categories = [
+    {
+        id:1,
+        image: beverages,
+        name: 'bevarages',
+    },
+    {
+        id: 2,
+        image: diary_and_eggs,
+        name: 'diary and eggs',
+    },
+    {
+        id: 3,
+        image: fresh_produce,
+        name: 'fresh produce',
+    },
+    {
+        id: 4,
+        image: meat_and_seafood,
+        name: 'meat and seafood',
+    },
+    {
+        id: 5,
+        image: frozen_foods,
+        name: 'frozen foods',
+    },
+]
+
+export default categories;
