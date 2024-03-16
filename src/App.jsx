d0915e5674d9e28974ebb371d1058e9fbb2099b9
Christopher Steven <christopherstevennunoo@gmail.com>
2024-03-16 10:36:37 +0000

    browser router and page outlets configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
 import './App.css'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 
-function App() {
-
-  return (
+const Layout = () => {
+  return(
     <div className='app'>
+      <h1>navbar</h1>
+      <Outlet />
+      <h1>footer</h1>
     </div>
   )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children:[
+      {
+        path: "/",
+        element: <h1>home</h1>
+      },
+      {
+        path: "/about",
+        element: <h1>about</h1>
+      }
+    ]
+  }
+  
+])
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App
