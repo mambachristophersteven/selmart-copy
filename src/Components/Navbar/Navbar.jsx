5cc1219c1a6b92864cfb35bf772df551179d023c
Christopher Steven <christopherstevennunoo@gmail.com>
2024-03-17 21:13:25 +0000

    calling array file working with map function

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,19 @@
 import styles from './Navbar.module.css'
+import categories from '../../assets/categories'
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
       <h1>hello</h1>
+      {
+        categories.map(categoryItem=>{
+          return(
+          <div key={categoryItem.id}>
+            <p>{categoryItem.name}, {categoryItem.id}</p>
+          </div>
+        )})
+
+      }
         
     </div>
   )
