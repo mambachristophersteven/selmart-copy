e92b4de650ac447bdfbcf2a366bdbdf44ded2c28
Christopher Steven <christopherstevennunoo@gmail.com>
2024-03-17 20:45:57 +0000

    navbar update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,10 @@
 import styles from './Navbar.module.css'
-import categories from '../../assets/categories'
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
-        {
-          categories.map((categoryItem,i)=>{
-              <div className={styles.categories} key={i}>
-                  <h1>{categoryItem.id}</h1>
-                  <h1>{categoryItem.name}</h1>
-                  <img src={categoryItem.image} alt="" />
-              </div>
-          })
-        }
+      <h1>hello</h1>
+        
     </div>
   )
 }
