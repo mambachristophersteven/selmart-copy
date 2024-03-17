a97832556c99b3daad52c7439db3e5b1a03f693f
Christopher Steven <christopherstevennunoo@gmail.com>
2024-03-17 21:40:09 +0000

    navbar content addition

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,49 @@
 import styles from './Navbar.module.css'
+import selmart_logo from '../../assets/selmart-logo.svg'
+import nav_on from '../../assets/nav-link.svg'
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
-      
+      <div className="logo">
+        <a href="#">
+          <img src={selmart_logo} alt="" />
+          <p className="logo-name">sel<span>mart</span></p>
+        </a>
+      </div>
+      <div className="nav-links">
+        <ul>
+          <li>
+            <a href="#">
+              <p>home</p>
+              <img src={nav_on} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>shop</p>
+              <img src={nav_on} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>categories</p>
+              <img src={nav_on} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <p>deals</p>
+              <img src={nav_on} alt="" />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="search-side-links">
+        <div className="search">
+          
+        </div>
+      </div>
       {/* {
         categories.map(categoryItem=>{
           return(
