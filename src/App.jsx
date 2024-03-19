761fff09ee05f2ba762a11f51b934224fe2317bd
Christopher Steven <christopherstevennunoo@gmail.com>
2024-03-19 10:51:52 +0000

    calling new page components in createBrowserRouter in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from './Components/Navbar/Navbar'
 import Footer from './Components/Footer/Footer'
 import Home from './Pages/Home/Home'
 import Shop from './Pages/Shop/Shop'
+import Categories from './Pages/Categories/Categories'
+import Deals from './Pages/Deals/Deals'
 
 const Layout = () => {
   return(
@@ -27,7 +29,15 @@ const router = createBrowserRouter([
       {
         path: "/shop",
         element: <Shop />
-      }
+      },
+      {
+        path: "/categories",
+        element: <Categories />
+      },
+      {
+        path: "/deals",
+        element: <Deals />
+      },
     ]
   }
   
