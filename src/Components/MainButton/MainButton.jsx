37dddfcbde3db214f6944d40c8f94bac41910b48
Christopher Steven <christopherstevennunoo@gmail.com>
2024-03-20 10:38:49 +0000

    MainButton component styling finished

--- src/Components/MainButton/MainButton.jsx
+++ src/Components/MainButton/MainButton.jsx
@@ -1,11 +1,13 @@
 import styles from './MainButton.module.css'
+import right_arrow from '../../assets/right-arrow.svg'
 /* eslint-disable react/prop-types */
 
 const MainButton = (props) => {
   return (
-    <div className={styles.main_button}>
+    <button className={styles.main_button}>
       <p className={styles.text}>{props.text}</p>
-    </div>
+      <img src={right_arrow} alt="" />
+    </button>
   )
 }
 
