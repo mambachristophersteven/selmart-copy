6a943c76fd4fd201e753667bef5362b5b5867994
Christopher Steven <christopherstevennunoo@gmail.com>
2024-03-20 13:40:45 +0000

    CategoryCard component initialized

---
+++ src/Components/CategoryCard/CategoryCard.jsx
@@ -0,0 +1,10 @@
+import styles from './CategoryCard.module.css'
+
+
+const CategoryCard = () => {
+  return (
+    <div className={styles.category_card}>CategoryCard</div>
+  )
+}
+
+export default CategoryCard
