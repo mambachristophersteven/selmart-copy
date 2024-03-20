c57391ebdf7db09d2a1b7f31faf8c3d2126e5658
Christopher Steven <christopherstevennunoo@gmail.com>
2024-03-20 12:26:18 +0000

    finished home frame styling

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import MainButton from '../../Components/MainButton/MainButton'
 import styles from './Home.module.css'
+import point1 from '../../assets/point1.png'
+import point2 from '../../assets/point2.png'
 
 const Home = () => {
   return (
@@ -13,7 +15,8 @@ const Home = () => {
           </div>
           <div className={styles.right}>
             <div className="right_content">
-              <p>right</p>
+              <img src={point1} className={styles.point1} alt="" />
+              <img src={point2} className={styles.point2} alt="" />
             </div>
           </div>
         </div>
