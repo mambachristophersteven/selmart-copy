c5f19e53d88eb53c38273ded36cf5c028eded776
Christopher Steven <christopherstevennunoo@gmail.com>
2024-03-20 09:54:52 +0000

    home banner content addition

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,14 @@ import styles from './Home.module.css'
 const Home = () => {
   return (
     <div className={styles.home}>
-        
+        <div className={styles.home_frame}>
+          <div className={styles.left}>
+            <p className={styles.save_cash}>Save up to 30% cash</p>
+            <p className={styles.main_text}>Groceries Made Fresh and Easy</p>
+            <p className={styles.sub_text}>Welcome to Selmart. Browse a Wide Selection of Fresh Produce, Pantry Staples, and More.</p>
+          </div>
+          <div className={styles.right}></div>
+        </div>
     </div>
   )
 }
