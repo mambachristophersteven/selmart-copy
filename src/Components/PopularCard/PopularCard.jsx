191fcc8ad4fbde448d6ffbe7a5c8e294a4822632
Christopher Steven <christopherstevennunoo@gmail.com>
2024-03-21 09:24:45 +0000

    initialized PopularCard component with styling file

---
+++ src/Components/PopularCard/PopularCard.jsx
@@ -0,0 +1,10 @@
+import styles from './PopularCard.module.css'
+
+
+const PopularCard = () => {
+  return (
+    <div className={styles.popular_card}>PopularCard</div>
+  )
+}
+
+export default PopularCard
