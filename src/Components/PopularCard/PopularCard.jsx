5adc87525f256f3eeb779d9dd1e5487aba3448ee
Christopher Steven <christopherstevennunoo@gmail.com>
2024-03-21 11:07:38 +0000

    PopularCard content added

--- src/Components/PopularCard/PopularCard.jsx
+++ src/Components/PopularCard/PopularCard.jsx
@@ -1,12 +1,35 @@
 import styles from './PopularCard.module.css'
+import favorite_icon from '../../assets/favorite.svg'
+import favorited_icon from '../../assets/favorited.svg'
+import button_cart_icon from '../../assets/button-cart-icon.svg'
 /* eslint-disable react/prop-types */
 
 
 const PopularCard = (props) => {
   return (
     <div className={styles.popular_card}>
-        <div className={styles.product_image}>
-            <img src={props.image} alt="" />
+        <div className={styles.product_image_section}>
+            <img className={styles.product_image} src={props.image} alt="" />
+            <div className={styles.favoriting_icons}>
+                <img className={styles.favorite} src={favorite_icon} alt="" />
+                <img className={styles.favorited} src={favorited_icon} alt="" />
+            </div>
+        </div>
+        <div className={styles.name_and_price}>
+            <div className={styles.name}>
+                <p className={styles.product_category}>{props.category}</p>
+                <p className={styles.product_name}>{props.name}</p>
+            </div>
+            <div className={styles.prices}>
+                <p className={styles.new_price}>{props.new_price}</p>
+                <p className={styles.old_price}>{props.old_price}</p>
+            </div>
+        </div>
+        <div className={styles.cart_button}>
+            <button>
+                <p className={styles.button_text}>add to cart</p>
+                <img src={button_cart_icon} alt="" />
+            </button>
         </div>
     </div>
   )
