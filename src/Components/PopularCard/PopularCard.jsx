f31e38de2a40c1cdcffbc85747c8d3c6ac9191c2
Christopher Steven <christopherstevennunoo@gmail.com>
2024-03-21 09:55:12 +0000

    beginning addition of content for PopularCard and adding favorite icons

--- src/Components/PopularCard/PopularCard.jsx
+++ src/Components/PopularCard/PopularCard.jsx
@@ -1,9 +1,14 @@
 import styles from './PopularCard.module.css'
+/* eslint-disable react/prop-types */
 
 
-const PopularCard = () => {
+const PopularCard = (props) => {
   return (
-    <div className={styles.popular_card}>PopularCard</div>
+    <div className={styles.popular_card}>
+        <div className={styles.product_image}>
+            <img src={props.image} alt="" />
+        </div>
+    </div>
   )
 }
 
