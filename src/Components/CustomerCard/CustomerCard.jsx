3d4435def713f6b78655480aeddd1554df92d41e
Christopher Steven <christopherstevennunoo@gmail.com>
2024-03-25 11:20:52 +0000

    CustomerCard initialized

---
+++ src/Components/CustomerCard/CustomerCard.jsx
@@ -0,0 +1,9 @@
+import styles from './CustomerCard.module.css'
+
+const CustomerCard = () => {
+  return (
+    <div className={styles.customer_card}>CustomerCard</div>
+  )
+}
+
+export default CustomerCard
