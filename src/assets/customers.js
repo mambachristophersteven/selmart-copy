7b4346c79d1b79be2adcde94cadbf09297ba9164
Christopher Steven <christopherstevennunoo@gmail.com>
2024-03-25 11:13:47 +0000

    customer data file created

---
+++ src/assets/customers.js
@@ -0,0 +1,27 @@
+import customer1 from '../assets/customer1.png'
+import customer2 from '../assets/customer2.png'
+import customer3 from '../assets/customer3.png'
+
+const flashDeals = [
+    {
+        id:1,
+        image: customer1,
+        name: 'berry young',
+        message: 'What are you waiting, browse through our hotest category and purchase your healthy groceries from Selmart.',
+    },
+    {
+        id:2,
+        image: customer2,
+        name: 'akua thompson',
+        message: 'What are you waiting, browse through our hotest category and purchase your healthy groceries from Selmart.',
+    },
+    {
+        id:3,
+        image: customer3,
+        name: 'jackie Folla ',
+        message: 'What are you waiting, browse through our hotest category and purchase your healthy groceries from Selmart.',
+    },
+
+]
+
+export default flashDeals;
