35c4e5de4ba0210bce1a7a934c8e94565a7ac7dc
Christopher Steven <christopherstevennunoo@gmail.com>
2024-03-25 22:41:01 +0000

    cuatomerCard content added

--- src/Components/CustomerCard/CustomerCard.jsx
+++ src/Components/CustomerCard/CustomerCard.jsx
@@ -1,8 +1,16 @@
 import styles from './CustomerCard.module.css'
+import quote from '../../assets/quote.svg'
+/* eslint-disable react/prop-types */
 
-const CustomerCard = () => {
+
+const CustomerCard = (props) => {
   return (
-    <div className={styles.customer_card}>CustomerCard</div>
+    <div className={styles.customer_card}>
+        <img className={styles.quote} src={quote} alt="" />
+        <img className={styles.customer_image} src={props.image} alt="" />
+        <p className={styles.customer_name}>{props.name}</p>
+        <p className={styles.customer_message}>{props.message}</p>
+    </div>
   )
 }
 
